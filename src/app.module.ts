8ae758237dc99b29bf686291ae23043dc1b1eefc
Satish Yadav <satishkumaryadav72@gmail.com>
2023-08-24 22:51:56 +0530

    DB config has been refactored and updated.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
+import appConfig from './config/app.config';
+import { ConnectOptions } from 'typeorm';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      load: [appConfig],
+    }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'astra.local',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return configService.get<ConnectOptions>('database');
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
