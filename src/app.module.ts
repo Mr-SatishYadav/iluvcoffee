5db373fb3507d82724d96b1ae4b962e5e7959356
yadavS <satish.yadav@nagarro.com>
2023-09-18 12:54:31 +0530

    Environment variable validation using JOI.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,26 @@
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConnectOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { CoffeesModule } from './coffees/coffees.module';
 import appConfig from './config/app.config';
-import { ConnectOptions } from 'typeorm';
-import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       cache: true,
       load: [appConfig],
+      validationSchema: Joi.object({
+        DB_HOST: Joi.required(),
+        DB_PORT: Joi.number().default(5432),
+        DB_NAME: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.required(),
+        DB_SYNCHRONIZE: Joi.optional().default(false),
+      }),
     }),
     CoffeesModule,
     TypeOrmModule.forRootAsync({
