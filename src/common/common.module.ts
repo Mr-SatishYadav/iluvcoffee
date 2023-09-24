81471b11ae72c2242a5465e86bafdd39c0ba34b5
yadavS <satish.yadav@nagarro.com>
2023-09-24 12:48:34 +0530

    Updated the middleware watch to a specif route.

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
 import { ConfigModule } from '@nestjs/config';
@@ -10,6 +15,8 @@ import { LoggingMiddleware } from './middleware/logging.middleware';
 })
 export class CommonModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggingMiddleware).forRoutes('*');
+    consumer
+      .apply(LoggingMiddleware)
+      .forRoutes({ path: 'coffees', method: RequestMethod.GET });
   }
 }
