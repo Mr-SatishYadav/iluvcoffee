358cb9cff3e6645a204dafe0268284f521ec357c
Satish Yadav <satish.yadav@nagarro.com>
2024-02-24 22:22:52 +0530

    Delete app.controller.ts and app.module.ts, and update main.ts

--- src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-@Module({
-  // Decorator for the module class
-  controllers: [AppController], // Module class
-})
-export class AppModule {}
