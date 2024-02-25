614e236624126fcbafc180a83173acaf5257a3c2
Satish Yadav <satish.yadav@nagarro.com>
2024-02-25 08:13:53 +0530

    Add validation pipe to the app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule); // Create the app with NestFactory
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); // Add a validation pipe to the app
   await app.listen(3000); // Listen on port 3000
 }
 bootstrap();
