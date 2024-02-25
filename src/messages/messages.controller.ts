21ffe53a86def627eeda105309d8a346e8138039
Satish Yadav <satish.yadav@nagarro.com>
2024-02-25 08:46:13 +0530

    Refactor messages controller and add type checking

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,9 +1,13 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 /**
  * Controller for managing messages.
  */
 @Controller('messages')
+/**
+ * Controller class for managing messages.
+ */
 export class MessagesController {
   /**
    * Get a list of messages.
@@ -16,6 +20,7 @@ export class MessagesController {
 
   /**
    * Get a single message by ID.
+   * @param id The ID of the message.
    * @returns A string representing a single message.
    */
   @Get('/:id')
@@ -25,10 +30,12 @@ export class MessagesController {
 
   /**
    * Create a new message.
+   * @param body The body of the request.
+   * @param body.content The content of the message.
    * @returns A string representing the created message.
    */
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     return 'Create a message with the content: ' + body.content;
   }
 }
