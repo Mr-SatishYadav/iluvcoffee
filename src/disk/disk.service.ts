fbb579f2e01a18bbddfc9801a725c7ef23c095c7
Satish Yadav <satish.yadav@nagarro.com>
2024-02-26 13:22:40 +0530

    Add DiskModule and DiskService

---
+++ src/disk/disk.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
+
+@Injectable()
+export class DiskService {
+  constructor(private readonly powerService: PowerService) {}
+
+    getManufacturer() {
+        return 'Seagate';
+    }
+
+    getDiskType() {
+        return 'SSD';
+    }
+
+    getData() {
+        console.log('Drawing 20 watts of power from Power service.')
+        this.powerService.supplyPower(20);
+        return 'Data!!!';
+    }
+}
