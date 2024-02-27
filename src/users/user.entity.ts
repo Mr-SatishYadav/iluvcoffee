8595adc81e46e1223c0a3a39582efc63833db0d6
Satish Yadav <satish.yadav@nagarro.com>
2024-02-27 20:08:43 +0530

    Remove name and role columns from User entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -6,23 +6,14 @@ export class User {
     id: number;
 
     @Column()
-    name: string;
-
-    @Column()
     email: string;
 
     @Column()
     password: string;
 
     @Column()
-    role: string;
-
-    @Column()
     created_at: Date;
     
     @Column()
     updated_at: Date;
-    
-    @Column()
-    deleted_at: Date;
 }
