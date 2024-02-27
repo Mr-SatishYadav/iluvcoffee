703618fd219b683797b9cca591b2c5b5bc43de37
Satish Yadav <satish.yadav@nagarro.com>
2024-02-27 21:46:50 +0530

    Refactor UsersService in users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { FindManyOptions, FindOneOptions, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
@@ -9,8 +9,40 @@ export class UsersService {
     @InjectRepository(User) private readonly repo: Repository<User>,
   ) {}
 
-    create(data: Partial<User>) {
-        const user = this.repo.create({...data, created_at: new Date(), updated_at: new Date()});
-        return this.repo.save(user);
+  create(data: Partial<User>) {
+    const user = this.repo.create({
+      ...data,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    const options = { where: { id } } as FindOneOptions<User>;
+    return this.repo.findOne(options);
+  }
+
+  find(email: string) {
+    const options = { where: { email } } as FindManyOptions<User>;
+    return this.repo.find(options);
+  }
+
+  async update(id: number, data: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`user not found with provided id:${id}`);
     }
+    Object.assign(user, data);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`user not found with provided id:${id}`);
+    }
+    await this.repo.remove(user);
+    return user;
+  }
 }
