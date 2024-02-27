074447b841772d774e15efd011922508503b2899
Satish Yadav <satish.yadav@nagarro.com>
2024-02-27 16:41:31 +0530

    Add TypeOrmModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { MessagesModule } from './messages/messages.module';
 import { ComputerModule } from './computer/computer.module';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [AppController],
-  imports: [MessagesModule, ComputerModule, UsersModule, ReportsModule],
+  imports: [MessagesModule, ComputerModule, UsersModule, ReportsModule, TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [__dirname + '/**/*.entity{.ts}'],
+    synchronize: true,
+  })],
 })
 export class AppModule {}
