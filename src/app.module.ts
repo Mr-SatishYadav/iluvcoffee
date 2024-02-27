3b06b5a375f490cd4ebec2989983be45abaec045
Satish Yadav <satish.yadav@nagarro.com>
2024-02-27 16:21:22 +0530

    Add UsersModule and ReportsModule imports to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MessagesModule } from './messages/messages.module';
 import { ComputerModule } from './computer/computer.module';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
   controllers: [AppController],
-  imports: [MessagesModule, ComputerModule],
+  imports: [MessagesModule, ComputerModule, UsersModule, ReportsModule],
 })
 export class AppModule {}
