ad7fa2be0d3c7dcdd978c5fb9acee9d9bafe661c
Satish Yadav <satish.yadav@nagarro.com>
2024-02-27 21:23:29 +0530

    Add logging hooks to User entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,19 +1,42 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterUpdate,
+  AfterRemove,
+} from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 
-    @Column()
-    created_at: Date;
-    
-    @Column()
-    updated_at: Date;
+  @Column()
+  created_at: Date;
+
+  @Column()
+  updated_at: Date;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('User has been inserted');
+    console.table({id: this.id, created_at: this.created_at.toLocaleString(), email: this.email});
+  }
+  @AfterUpdate()
+  logUpdate() {
+    console.log('User has been updated');
+    console.table({id: this.id, updated_at: this.updated_at.toLocaleString(), email: this.email});
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log('User has been removed');
+    console.table({id: this.id, updated_at: this.updated_at.toLocaleString(), email: this.email});
+  }
 }
