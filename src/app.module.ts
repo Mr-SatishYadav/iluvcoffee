bd0e8ae188478252888fe063bf57e67e61b72279
Satish Yadav <satish.yadav@nagarro.com>
2024-02-27 16:52:24 +0530

    Refactor app.module.ts imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { MessagesModule } from './messages/messages.module';
-import { ComputerModule } from './computer/computer.module';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { ComputerModule } from './computer/computer.module';
+import { MessagesModule } from './messages/messages.module';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [AppController],
