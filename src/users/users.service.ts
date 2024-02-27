02ddf054e661c628e6f8c7c3606f587ec746bb17
Satish Yadav <satish.yadav@nagarro.com>
2024-02-27 20:09:03 +0530

    Add create method to UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly repo: Repository<User>,
+  ) {}
+
+    create(data: Partial<User>) {
+        const user = this.repo.create({...data, created_at: new Date(), updated_at: new Date()});
+        return this.repo.save(user);
+    }
+}
