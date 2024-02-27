4a39b35bf0d569d6bedc6b14b5de2a53c32f4f5e
Satish Yadav <satish.yadav@nagarro.com>
2024-02-27 20:08:32 +0530

    Update validation pipe options

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); // Create the app with NestFactory
-  app.useGlobalPipes(new ValidationPipe({ transform: true })); // Add a validation pipe to the app
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true })); // Add a validation pipe to the app
   await app.listen(3000); // Listen on port 3000
 }
 bootstrap();
