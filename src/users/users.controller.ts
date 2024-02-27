53d14f3dfd9764928863a4d3bc36f6eafbc62b45
Satish Yadav <satish.yadav@nagarro.com>
2024-02-27 22:21:57 +0530

    Add CRUD operations for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -10,4 +11,25 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body);
   }
+
+  @Patch('/:id')
+  updateUser(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateUserDto) {
+    return this.usersService.update(id, body);
+  }
+
+  @Get('/:id')
+  getUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Get()
+  getAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.remove(id);
+  }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,20 @@ import { FindManyOptions, FindOneOptions, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
+/**
+ * Service responsible for handling user-related operations.
+ */
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly repo: Repository<User>,
   ) {}
 
+  /**
+   * Creates a new user.
+   * @param data - The data of the user to be created.
+   * @returns The created user.
+   */
   create(data: Partial<User>) {
     const user = this.repo.create({
       ...data,
@@ -18,30 +26,57 @@ export class UsersService {
     return this.repo.save(user);
   }
 
+  /**
+   * Finds a user by ID.
+   * @param id - The ID of the user to find.
+   * @returns The found user.
+   * @throws NotFoundException if the user is not found.
+   */
   findOne(id: number) {
     const options = { where: { id } } as FindOneOptions<User>;
-    return this.repo.findOne(options);
-  }
-
-  find(email: string) {
-    const options = { where: { email } } as FindManyOptions<User>;
-    return this.repo.find(options);
-  }
-
-  async update(id: number, data: Partial<User>) {
-    const user = await this.findOne(id);
+    const user = this.repo.findOne(options);
     if (!user) {
       throw new NotFoundException(`user not found with provided id:${id}`);
     }
-    Object.assign(user, data);
+    return user;
+  }
+
+  /**
+   * Finds users by email.
+   * @param email - The email of the users to find.
+   * @returns The found users.
+   * @throws NotFoundException if no users are found with the provided email.
+   */
+  find(email: string) {
+    const options = { where: { email } } as FindManyOptions<User>;
+    const users = this.repo.find(options);
+    if (!users) {
+      throw new NotFoundException(`user[s] not found with provided email:${email}`);
+    }
+    return users;
+  }
+
+  /**
+   * Updates a user by ID.
+   * @param id - The ID of the user to update.
+   * @param data - The updated data of the user.
+   * @returns The updated user.
+   * @throws NotFoundException if the user is not found.
+   */
+  async update(id: number, data: Partial<User>) {
+    const user = await this.findOne(id);
+    Object.assign(user, { ...data, updated_at: new Date() });
     return this.repo.save(user);
   }
 
+  /**
+   * Removes a user by ID.
+   * @param id - The ID of the user to remove.
+   * @returns The removed user.
+   * @throws NotFoundException if the user is not found.
+   */
   async remove(id: number) {
     const user = await this.findOne(id);
-    if (!user) {
-      throw new NotFoundException(`user not found with provided id:${id}`);
-    }
     await this.repo.remove(user);
     return user;
   }
