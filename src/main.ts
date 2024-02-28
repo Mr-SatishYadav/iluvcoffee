8891d2265cb752ef62aba765640ac12c187e38b6
Satish Yadav <satish.yadav@nagarro.com>
2024-02-28 21:46:54 +0530

    Add cookie session middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession =  require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); // Create the app with NestFactory
+  app.use(
+    cookieSession({
+      keys: ['asdfasfs'],
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true })); // Add a validation pipe to the app
   await app.listen(3000); // Listen on port 3000
 }
