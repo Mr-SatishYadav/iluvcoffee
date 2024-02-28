26c39a7b5ef4e15834d0aa796cb354d4442a304b
Satish Yadav <satish.yadav@nagarro.com>
2024-02-28 15:47:10 +0530

    Add error handling to getUser and getAllUsers methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -13,23 +24,35 @@ export class UsersController {
   }
 
   @Patch('/:id')
-  updateUser(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateUserDto) {
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateUserDto,
+  ) {
     return this.usersService.update(id, body);
   }
 
   @Get('/:id')
-  getUser(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findOne(id);
+  async getUser(@Param('id', ParseIntPipe) id: number) {
+    const user = await this.usersService.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`user not found with provided id:${id}`);
+    }
+    return user;
   }
 
   @Get()
-  getAllUsers(@Query('email') email: string) {
-    return this.usersService.find(email);
+  async getAllUsers(@Query('email') email: string) {
+    const users = await this.usersService.find(email);
+    if (users.length === 0) {
+      throw new NotFoundException(
+        `user not found with provided email:${email}`,
+      );
+    }
+    return users;
   }
 
   @Delete('/:id')
   removeUser(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(id);
   }
-
 }
