93ecc8df6c61ff1992aad31fba3a1cd75adf81ba
Satish Yadav <satish.yadav@nagarro.com>
2024-02-29 10:31:58 +0530

    Add DevtoolsModule to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,24 @@ import { ComputerModule } from './computer/computer.module';
 import { MessagesModule } from './messages/messages.module';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
   controllers: [AppController],
-  imports: [MessagesModule, ComputerModule, UsersModule, ReportsModule, TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-  })],
+  imports: [
+    MessagesModule,
+    ComputerModule,
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const cookieSession =  require('cookie-session');
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule); // Create the app with NestFactory
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+  }); // Create the app with NestFactory
   app.use(
     cookieSession({
       keys: ['asdfasfs'],
