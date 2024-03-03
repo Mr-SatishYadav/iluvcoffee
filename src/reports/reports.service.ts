054b3233b535f1de3afc258c910c295cb50f1ca7
Satish Yadav <satish.yadav@nagarro.com>
2024-03-03 10:52:57 +0530

    Refactor reports module and controller

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './entities/report.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private readonly repo: Repository<Report>,
+  ) {}
+
+    create(data: Partial<Report>) {
+        const report = this.repo.create(data);
+        return this.repo.save(report);
+    }
+}
