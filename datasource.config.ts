4134febf872d79307221684103b74cadde87352f
Satish Yadav <satish.yadav@nagarro.com>
2024-03-08 09:11:57 +0530

    Remove unused database configuration file and update TypeORM configuration

---
+++ datasource.config.ts
@@ -0,0 +1,42 @@
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+const dbConfig = {
+  synchronize: false,
+  type: 'sqlite',
+} as DataSourceOptions;
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity.{js}'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      database: 'test.sqlite',
+      entities: [__dirname + '/**/*.entity.{ts}'],
+    });
+    break;
+  case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
+      ssl: true,
+    });
+    break;
+  default:
+    throw new Error('No database configuration for this environment');
+}
+
+export const dataSource = new DataSource(dbConfig);
+
+export const TypeOrmConfig = TypeOrmModule.forRootAsync({
+  useFactory: async (): Promise<TypeOrmModuleOptions> => ({
+    ...dbConfig,
+  }),
+});
+
+// module.exports = dbConfig;
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from 'datasource.config';
 import { AppController } from './app.controller';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
@@ -17,22 +17,9 @@ const cookieSession = require('cookie-session');
       cache: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    // MessagesModule,
-    // ComputerModule,
     UsersModule,
     ReportsModule,
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: configService.get<string>('DB_NAME'),
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          synchronize: true,
-        };
-      },
-      inject: [ConfigService],
-    }),
+    TypeOrmConfig,
     DevtoolsModule.register({
       http: process.env.NODE_ENV !== 'production',
     }),
